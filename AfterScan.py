65ec442b8b11948a6332a5dca27a3a31a4ca4707
jareff-g <juan.remirezdeesparza@gmail.com>
2024-01-05 13:50:45 +0100

    AfterScan 1.8.23 - Change 'get_best_template_size' so that it finds the best template bny resizing the template and not the image

--- AfterScan.py
+++ AfterScan.py
@@ -1561,43 +1561,36 @@ def get_best_template_size(img):
     else:
         hole_template_filename = hole_template_filename_r8
     template = cv2.imread(hole_template_filename)
-    template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-    template = cv2.Canny(template, 50, 200)
-    (tH, tW) = template.shape[:2]
     # cv2.imshow("Template", template)
     # Handle image to be searched
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edged = cv2.Canny(gray, 50, 200)
     found = None
-    # loop over the scales of the image
-    for scale in np.linspace(0.2, 1.0, 20)[::-1]:
-        # resize the image according to the scale, and keep track
-        # of the ratio of the resizing
-        #resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
-        resized = resize_image(gray, scale * 100)
-        r = gray.shape[1] / float(resized.shape[1])
-        # if the resized image is smaller than the template, then break
-        # from the loop
-        if resized.shape[0] < tH or resized.shape[1] < tW:
-            break
-        # detect edges in the resized, grayscale image and apply template
-        # matching to find the template in the image
-        edged = cv2.Canny(resized, 50, 200)
-        result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
-        (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
-        # check to see if the iteration should be visualized
-        # if we have found a new maximum correlation value, then update
-        # the bookkeeping variable
+    # loop over the scales of the template
+    for scale in np.linspace(0.6, 2.0, 20)[::-1]:
+        # resize the image according to the scale, and keep track of the ratio of the resizing
+        resized = resize_image(template, scale * 100)
+        # if the resized template is bigger than the image, skip to next (should be smaller)
+        if resized.shape[0] > img.shape[0] or resized.shape[1] > img.shape[1]:
+            continue
+        tgray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+        template_aux = cv2.Canny(tgray, 50, 200)
+        # detect edges in the resized, grayscale image and apply template matching to find the template in the image
+        result = cv2.matchTemplate(edged, template_aux, cv2.TM_CCOEFF)
+        (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(result)
+        print(f"maxVal {maxVal}, maxLoc {maxLoc}, TSize {template_aux.shape[1]}, {template_aux.shape[0]}")
+        # check to see if the iteration should be visualized if we have found a new maximum correlation value,
+        # then update the bookkeeping variable
         if found is None or maxVal > found[0]:
-            found = (maxVal, maxLoc, r)
+            found = (maxVal, maxLoc, template_aux.shape[0])
 
+    print(f"Found: maxVal {found[0]}")
     # unpack the bookkeeping variable and compute the (x, y) coordinates
     # of the bounding box based on the resized ratio
-    (_, maxLoc, r) = found
-    (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-    (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
-    # Optimize template rectangle: Maximize horizontally, and for S8, make it taller
-    startX = 0
-    endX = img.shape[1]-1
+    (_, maxLoc, template_height) = found
+    (tH, tW) = template_aux.shape[:2]
+    (startX, startY) = (0, maxLoc[1])
+    (endX, endY) = (img.shape[1]-1, maxLoc[1] + template_height)
     """
     if film_type.get() == 'S8':
         startY = startY - template.shape[0]
